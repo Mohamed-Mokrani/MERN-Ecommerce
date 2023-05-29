33df836b0e9cce14da5ee01d07a536b88514c700
Mohamed-Mokrani <med.mokranii@hotmail.fr>
2023-05-29 21:41:47 +0100

    update

--- backend/middleWare/authMiddleWare.js
+++ backend/middleWare/authMiddleWare.js
@@ -6,6 +6,7 @@ const protect = expressAsyncHandler(async (req, res, next) => {
   try {
     const token = req.cookies.token
 
+    
     if (!token) {
       res.status(401)
       throw new Error("Not authorized, Please Login")

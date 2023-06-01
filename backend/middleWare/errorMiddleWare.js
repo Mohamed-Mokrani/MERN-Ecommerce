23d94fba593b62c0fe79a6948537e15e26e71e4f
Mohamed-Mokrani <med.mokranii@hotmail.fr>
2023-06-01 20:02:36 +0100

    update

--- backend/middleWare/errorMiddleWare.js
+++ backend/middleWare/errorMiddleWare.js
@@ -1,5 +1,5 @@
 const errorHandler = (err, req, res, next) => {
-  
+
   const statusCode = res.statusCode ? res.statusCode : 500
   res.status(statusCode)
   res.json({
@@ -7,4 +7,5 @@ const errorHandler = (err, req, res, next) => {
     stack: process.env.NODE_ENV === "development" ? err.stack : null,
   })
 }
+
 module.exports = errorHandler

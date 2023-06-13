d2807e0081a99a1d883352f125334e7d01b3e1ed
Mohamed-Mokrani <med.mokranii@hotmail.fr>
2023-06-13 12:23:30 +0100

    Update

--- backend/controllers/contactController.js
+++ backend/controllers/contactController.js
@@ -24,7 +24,16 @@ const contactUs = asyncHandler(async (req, res) => {
   const send_from = process.env.EMAIL_USER
   const reply_to = user.email
 
-  
+  try {
+    await sendEmail(subject, message, send_to, send_from, reply_to)
+    res.status(200).json({
+      success: true,
+      message: "Email Sent"
+    })
+  } catch (error) {
+    res.status(500)
+    throw new Error("Email not sent,please try again!")
+  }
 })
 
 module.exports = {

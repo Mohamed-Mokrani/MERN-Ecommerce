77c7a176647977cd94c44d66bfa9859e59c606af
Mohamed MOKRANI <119493044+Mohamed-Mokrani@users.noreply.github.com>
2023-09-03 09:25:50 +0100

    Update authMiddleWare.js

--- backend/middleWare/authMiddleWare.js
+++ backend/middleWare/authMiddleWare.js
@@ -2,6 +2,7 @@ const expressAsyncHandler = require("express-async-handler")
 const jwt = require("jsonwebtoken")
 const User = require("../models/userModel")
 
+
 const protect = expressAsyncHandler(async (req, res, next) => {
   try {
     const token = req.cookies.token
